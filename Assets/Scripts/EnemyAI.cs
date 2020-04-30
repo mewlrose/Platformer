89b0fd9c2268b234b602701ee82e6c3ccef4de18
Melanie <xu.melanie@yahoo.ca>
2020-04-29 22:19:16 -0700

    first commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public float gravity;
+    public Vector2 velocity;
+    public bool isWalkingLeft = true;
+    private bool grounded = false;
+    public LayerMask floorMask;
+    public LayerMask wallMask;
+
+    private enum EnemyState
+    {
+        walking,
+        falling,
+        dead
+    }
+
+    private EnemyState state = EnemyState.falling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enabled = false;
+        Fall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateEnemyPosition();
+    }
+
+    void UpdateEnemyPosition()
+    {
+        if (state != EnemyState.dead)
+        {
+            Vector3 pos = transform.localPosition;
+            Vector3 scale = transform.localScale;
+
+            if (state == EnemyState.falling)
+            {
+                pos.y += velocity.y * Time.deltaTime;
+                velocity.y -= gravity * Time.deltaTime;
+            }
+            if (state == EnemyState.walking)
+            {
+                if (isWalkingLeft)
+                {
+                    pos.x -= velocity.x * Time.deltaTime;
+                    scale.x = -1;
+                }
+                else
+                {
+                    pos.x += velocity.x * Time.deltaTime;
+                    scale.x = 1;
+                }
+            }
+            if (velocity.y <= 0)
+            {
+                pos = CheckGround(pos);
+            }
+            CheckWalls(pos, scale.x);
+            
+            transform.localPosition = pos;
+            transform.localScale = scale;
+        }
+    }
+
+    Vector3 CheckGround(Vector3 pos)
+    {
+        Vector2 originLeft = new Vector2(pos.x - 0.5f + 0.2f, pos.y - 0.5f);
+        Vector2 originMid = new Vector2(pos.x, pos.y - 0.5f);
+        Vector2 originRight = new Vector2(pos.x + 0.5f - 0.2f, pos.y - 0.5f);
+
+        RaycastHit2D groundLeft = Physics2D.Raycast(originLeft, Vector2.down, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+        
+        RaycastHit2D groundMid = Physics2D.Raycast(originMid, Vector2.down, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+        
+        RaycastHit2D groundRight = Physics2D.Raycast(originRight, Vector2.down, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+        
+        if (groundLeft.collider != null || groundMid.collider != null || groundRight.collider != null)
+        {
+            RaycastHit2D hitRay = groundLeft;
+            if (groundLeft)
+            {
+                hitRay = groundLeft;
+            } else if (groundMid)
+            {
+                hitRay = groundMid;
+            } else if (groundRight)
+            {
+                hitRay = groundRight;
+            }
+            pos.y = hitRay.collider.bounds.center.y + hitRay.collider.bounds.size.y/2 + 0.5f;
+            grounded = true;
+            velocity.y = 0;
+            state = EnemyState.walking;
+        } else
+        {
+            if (state != EnemyState.falling)
+            {
+                Fall();
+            }
+        }
+        return pos;
+    }
+
+    void CheckWalls(Vector3 pos, float direction)
+    {
+        Vector2 originTop = new Vector2(pos.x + direction * 0.4f, pos.y + 0.5f - 0.2f);
+        Vector2 originMid = new Vector2(pos.x + direction * 0.4f, pos.y);
+        Vector2 originBottom = new Vector2(pos.x + direction * 0.4f, pos.y - 0.5f + 0.2f);
+
+        RaycastHit2D wallTop = Physics2D.Raycast(   originTop, new Vector2(direction, 0), 
+                                                    velocity.x * Time.deltaTime, wallMask);
+
+        RaycastHit2D wallMid = Physics2D.Raycast(   originMid, new Vector2(direction, 0), 
+                                                    velocity.x * Time.deltaTime, wallMask);
+
+        RaycastHit2D wallBottom = Physics2D.Raycast(   originBottom, new Vector2(direction, 0), 
+                                                    velocity.x * Time.deltaTime, wallMask);
+
+        if (wallTop.collider != null || wallMid.collider != null || wallBottom.collider != null)
+        {
+            RaycastHit2D hitRay = wallTop;
+            if (wallTop)
+            {
+                hitRay = wallTop;
+            } else if (wallMid)
+            {
+                hitRay = wallMid;
+            } else if (wallBottom)
+            {
+                hitRay = wallBottom;
+            }
+
+            isWalkingLeft = !isWalkingLeft;
+        }
+    }
+
+    private void OnBecameVisible()
+    {
+        enabled = true;
+    }
+    private void onBecomeInvisible()
+    {
+        enabled = true;
+    }
+
+    void Fall()
+    {
+        velocity.y = 0;
+        state = EnemyState.falling;
+        grounded = false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpVelocity;
+    public Vector2 velocity;
+    public float gravity;
+    public LayerMask wallMask;
+    public LayerMask floorMask;
+    private bool walk, walkLeft, walkRight, jump;
+
+    public enum PlayerState
+    {
+        jumping,
+        idle,
+        walking
+    }
+
+    private PlayerState playerState = PlayerState.idle;
+    private bool grounded = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        CheckPlayerInput();
+        UpdatePlayerPosition();
+        UpdateAnimationStates();
+    }
+
+    void UpdatePlayerPosition()
+    {
+        Vector3 pos = transform.localPosition;
+        Vector3 scale = transform.localScale;
+
+        if (walk)
+        {
+            if (walkLeft)
+            {
+                pos.x -= (velocity.x * Time.deltaTime);
+                scale.x = -1;
+            }
+            if (walkRight)
+            {
+                pos.x += (velocity.x * Time.deltaTime);
+                scale.x = 1;
+            }
+            pos = CheckWallRays(pos, scale.x);
+        }
+
+        if (jump && playerState != PlayerState.jumping)
+        {
+            playerState = PlayerState.jumping;
+            velocity = new Vector2(velocity.x, jumpVelocity);
+        }
+
+        if (playerState == PlayerState.jumping)
+        {
+            pos.y += velocity.y * Time.deltaTime;
+            velocity.y -= gravity * Time.deltaTime;
+        }
+
+        if (velocity.y <= 0)
+        {
+            pos = CheckFloorRays(pos);
+        }
+
+        if (velocity.y >= 0)
+        {
+            pos = CheckCeilingRays(pos);
+        }
+
+        transform.localPosition = pos;
+        transform.localScale = scale;
+    }
+
+    void UpdateAnimationStates()
+    {
+        if (grounded && !walk)
+        {
+            GetComponent<Animator>().SetBool("isJumping", false);
+            GetComponent<Animator>().SetBool("isRunning", false);
+        }
+
+        if (grounded && walk)
+        {
+            GetComponent<Animator>().SetBool("isJumping", false);
+            GetComponent<Animator>().SetBool("isRunning", true);
+        }
+
+        if (playerState == PlayerState.jumping)
+        {
+            GetComponent<Animator>().SetBool("isJumping", true);
+            GetComponent<Animator>().SetBool("isRunning", false);
+        }
+    }
+
+    void CheckPlayerInput()
+    {
+        bool inputLeft = Input.GetKey(KeyCode.A);
+        bool inputRight = Input.GetKey(KeyCode.D);
+        bool inputSpace = Input.GetKey(KeyCode.S);
+
+        walk = inputLeft || inputRight;
+        walkLeft = inputLeft && !inputRight;
+        walkRight = !inputLeft && inputRight;
+        jump = inputSpace;
+    }
+
+    Vector3 CheckWallRays (Vector3 pos, float direction)
+    {
+        Vector2 originTop = new Vector2(pos.x + direction * 0.4f, pos.y + 1f - 0.2f);
+        Vector2 originMiddle = new Vector2(pos.x + direction * 0.4f, pos.y);
+        Vector2 originBottom = new Vector2(pos.x + direction * 0.4f, pos.y - 1f + 0.2f);
+
+        RaycastHit2D wallTop = Physics2D.Raycast(   originTop, new Vector2(direction, 0), 
+                                                    velocity.x * Time.deltaTime, wallMask);
+
+        RaycastHit2D wallMiddle = Physics2D.Raycast(originMiddle, new Vector2(direction, 0), 
+                                                    velocity.x * Time.deltaTime, wallMask);
+
+        RaycastHit2D wallBottom = Physics2D.Raycast(originBottom, new Vector2(direction, 0), 
+                                                    velocity.x * Time.deltaTime, wallMask);
+        
+        if (wallTop.collider != null || wallMiddle.collider != null || wallBottom.collider != null)
+        {
+            pos.x -= velocity.x * Time.deltaTime * direction;
+        }
+        return pos;
+    }
+
+    Vector3 CheckFloorRays (Vector3 pos)
+    {
+        Vector2 originLeft = new Vector2 (pos.x - 0.5f + 0.2f, pos.y - 1f);
+        Vector2 originMid = new Vector2 (pos.x, pos.y - 1f);
+        Vector2 originRight = new Vector2 (pos.x + 0.5f - 0.2f, pos.y - 1f);
+
+        RaycastHit2D floorLeft = Physics2D.Raycast( originLeft, Vector2.down, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+
+        RaycastHit2D floorMid = Physics2D.Raycast(  originMid, Vector2.down, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+
+        RaycastHit2D floorRight = Physics2D.Raycast(originRight, Vector2.down, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+        
+        if (floorLeft.collider != null || floorMid.collider != null || floorRight.collider != null)
+        {
+            RaycastHit2D hitRay = floorRight;
+            if (floorLeft)
+            {
+                hitRay = floorLeft;
+            } else if (floorMid)
+            {
+                hitRay = floorMid;
+            } else if (floorRight)
+            {
+                hitRay = floorRight;
+            }
+            
+            playerState = PlayerState.idle;
+            grounded = true;
+            velocity.y = 0;
+            pos.y = hitRay.collider.bounds.center.y + hitRay.collider.bounds.size.y/2 + 1;
+        }
+        else
+        {
+            if (playerState != PlayerState.jumping)
+            {
+                Fall();
+            }
+        }
+        return pos;
+    }
+
+    Vector3 CheckCeilingRays(Vector3 pos)
+    {
+        Vector2 originLeft = new Vector2 (pos.x - 0.5f + 0.2f, pos.y + 1f);
+        Vector2 originMid = new Vector2 (pos.x, pos.y + 1f);
+        Vector2 originRight = new Vector2 (pos.x + 0.5f - 0.2f, pos.y + 1f);
+
+        RaycastHit2D ceilLeft = Physics2D.Raycast( originLeft, Vector2.up, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+
+        RaycastHit2D ceilMid = Physics2D.Raycast(  originMid, Vector2.up, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+
+        RaycastHit2D ceilRight = Physics2D.Raycast(originRight, Vector2.up, 
+                                                    velocity.y * Time.deltaTime, floorMask);
+
+        if (ceilLeft.collider != null || ceilMid.collider != null || ceilRight.collider != null)
+        {
+            RaycastHit2D hitRay = ceilLeft;
+            if (ceilLeft)
+            {
+                hitRay = ceilLeft;
+            } else if (ceilMid)
+            {
+                hitRay = ceilMid;
+            } else if (ceilRight)
+            {
+                hitRay = ceilRight;
+            }
+            pos.y = hitRay.collider.bounds.center.y - hitRay.collider.bounds.size.y/2-1;
+            Fall();
+        }
+        return pos;
+    }
+
+    void Fall()
+    {
+        velocity.y = 0;
+        playerState = PlayerState.jumping;
+        grounded = false;
+    }
+}
